15352c4902989e9386bcc3d2d2d363a8e47ee360
NickSealer <nickfontechav@gmail.com>
2022-01-31 19:43:45 -0500

    fix seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,16 @@ end
 
 def lunches
   Lunch.create!([
-    {name: 'Chicken and waffles', description: '', scheduled_to: '31-01-2022'},
-    {name: 'Tacos', description: '', scheduled_to: '1-02-2022'},
-    {name: 'Curry' description: '', scheduled_to: '2-02-2022'},
-    {name: 'Pizza', description: '', scheduled_to: '3-02-2022'},
-    {name: 'Suchi', description: '', scheduled_to: '4-02-2022'},
-    {name: 'Breakfast for lunch', description: '', scheduled_to: '7-02-2022'},
-    {name: 'Hamburgers', description: '', scheduled_to: '8-02-2022'},
-    {name: 'Spaghetti', description: '', scheduled_to: '9-02-2022'},
-    {name: 'Salmon', description: '', scheduled_to: '10-02-2022'},
-    {name: 'Sandwiches', description: '', scheduled_to: '11-02-2022'},
+    {name: 'Chicken and waffles', description: 'Chicken and waffles', scheduled_to: '31-01-2022'},
+    {name: 'Tacos', description: 'Tacos', scheduled_to: '1-02-2022'},
+    {name: 'Curry', description: 'Curry', scheduled_to: '2-02-2022'},
+    {name: 'Pizza', description: 'Pizza', scheduled_to: '3-02-2022'},
+    {name: 'Suchi', description: 'Suchi', scheduled_to: '4-02-2022'},
+    {name: 'Breakfast for lunch', description: 'Breakfast for lunch', scheduled_to: '7-02-2022'},
+    {name: 'Hamburgers', description: 'Hamburgers', scheduled_to: '8-02-2022'},
+    {name: 'Spaghetti', description: 'Spaghetti', scheduled_to: '9-02-2022'},
+    {name: 'Salmon', description: 'Salmon', scheduled_to: '10-02-2022'},
+    {name: 'Sandwiches', description: 'Sandwiches', scheduled_to: '11-02-2022'},
   ])
 end
 
